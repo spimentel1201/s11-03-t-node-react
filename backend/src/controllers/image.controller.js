55f21aea3b8502835e07b9f5cd6aae31ccd60f44
cereceda1991 <cereceda1991@gmail.com>
2023-10-04 22:50:20 -0500

    feat: add new entity images

---
+++ backend/src/controllers/image.controller.js
@@ -0,0 +1,27 @@
+import cloudinary from "../config/cloudinary-config";
+import { Stream } from "stream";
+
+export const uploadImage = async (req, res) => {
+  try {
+    // Crea un flujo a partir del búfer
+    const stream = new Stream.PassThrough();
+    stream.end(req.file.buffer);
+
+    // Sube el flujo a Cloudinary utilizando upload_stream
+    const result = await cloudinary.uploader.upload_stream((error, result) => {
+      if (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error al cargar la imagen' });
+      } else {
+        // Devuelve la URL de la imagen subida en la respuesta
+        res.json({ photo_url: result.secure_url });
+      }
+    });
+
+    // Pasa el flujo al método upload_stream
+    stream.pipe(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al cargar la imagen' });
+  }
+};
--- backend/src/schemas/client.schema.js
+++ backend/src/schemas/client.schema.js
@@ -7,7 +7,7 @@ const clientSchemaDefinition = new mongoose.Schema({
   last_name: { type: String, default: 'Doe' },
   phone: { type: String, default: '555-123-4567' },
   address: { type: String, default: '123 Main Street' },
-  photo_url: { type: String, default: 'https://i.ibb.co/Zgq29Gy/profile.png' },
+  photo_url: { type: String, default: 'https://res.cloudinary.com/dxq0pypxu/image/upload/v1696476957/nn12qmebo7v6qhbwbkdf.png' },
   isActive: { type: Boolean, default: true },
 });
 
