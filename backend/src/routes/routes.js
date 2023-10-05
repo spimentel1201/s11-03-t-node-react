f16edd93dd806cff5e66d5ee6e977d6c1d65ebbf
Sonny <spimentelm1201@gmail.com>
2023-10-05 09:38:34 -0500

    feat pets schemas, routes and controller added

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import clientRoutes from './client.routes';
-import imageRoutes from './images.routes'
+import imageRoutes from './images.routes';
+import petRoutes from './pet.routes';
 
 const router = express.Router();
 
@@ -8,6 +9,8 @@ const router = express.Router();
 router.use('/api/v1/clients', clientRoutes);
 // Rutas de Imagenes
 router.use('/api/v1/images', imageRoutes);
+// Rutas de mascotas
+router.use('/api/v1/pets', petRoutes);
 // Otras rutas 
 
 export default router;
