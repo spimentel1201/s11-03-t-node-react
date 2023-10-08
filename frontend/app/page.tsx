cff458eeb6b34c6fb8c0970986b9da4ff1c9cc4a
diegonicita <diegonicita@gmail.com>
2023-10-08 17:21:14 -0300

    Add two test screens: login and register

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,95 +1,41 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-screen flex flex-col text-center items-center justify-center">
+      <div className="card lg:card-side bg-base-100 shadow-xl">
+        <figure className="p-4">
+          <Image src="/logo.png" alt="Album" width={200} height={200} />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Bienvenido a la Veterinaria</h2>
+          <p>Haz click en los botones para continuar navegando</p>
+          <div className="flex flex-row gap-4">
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">
+                <Link href="home">Home</Link>
+              </button>
+            </div>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">
+                <Link href="about">About</Link>
+              </button>
+            </div>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">
+                <Link href="login">Login</Link>
+              </button>
+            </div>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">
+                <Link href="register">Register</Link>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
