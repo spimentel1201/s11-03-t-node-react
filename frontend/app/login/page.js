9c911a493c9b2fe8e5fb8b7bc0ac5b0c33049c74
diegonicita <diegonicita@gmail.com>
2023-10-09 19:03:29 -0300

    Add styles to login and register

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -1,42 +1,62 @@
+import Image from 'next/image'
+
 const Login = () => (
-  <div className="hero-content flex-col lg:flex-row-reverse h-screen">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-        excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
-        id nisi.
-      </p>
-    </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-300">
-      <div className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input
-            type="text"
-            placeholder="email"
-            className="input input-bordered"
-          />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input
-            type="text"
-            placeholder="password"
-            className="input input-bordered"
-          />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">
-              Forgot password?
-            </a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-neutral">Login</button>
+  <div class="flex justify-center items-center h-screen">
+    <div className="hero-content flex-col lg:flex-row-reverse justify-start lg:justify-center">
+      <div className="text-center lg:text-left">
+        <Image
+          src="/auth-image.png"
+          alt="Imagen de perritos"
+          width="450"
+          height="500"
+        />
+        {/* <p className="py-6">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+          id nisi.
+        </p> */}
+      </div>
+      <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-300">
+        <div className="card-body">
+          <h1 className="text-3xl font-bold">Iniciar Sesión</h1>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">E-mail</span>
+            </label>
+            <input
+              type="text"
+              placeholder="vetcare@gmail"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Contraseña</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Ingrese tu contraseña"
+              className="input input-bordered"
+            />
+          </div>
+          <div class="flex justify-end">¿Olviste la contraseña?</div>
+          <div className="form-control mt-6">
+            <button className="btn btn-neutral">Iniciar Sesión</button>
+          </div>
+          <div class="divider">
+            O iniciar sesión con
+          </div>
+          <div className="avatar justify-center gap-8">
+            <div className="rounded">
+              <Image src="/logo.png" width="24" height="24" alt="icono" />
+            </div>
+            <div className="rounded">
+              <Image src="/logo.png" width="24" height="24" alt="icono" />
+            </div>
+            <div className="rounded">
+              <Image src="/logo.png" width="24" height="24" alt="icono" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- frontend/app/register/page.js
+++ frontend/app/register/page.js
@@ -1,51 +1,85 @@
+import Image from 'next/image'
+
 const Register = () => (
-    <div className="hero-content flex-col lg:flex-row-reverse h-screen">
-      <div className="text-center lg:text-left">
-        <h1 className="text-5xl font-bold">Register now!</h1>
-        <p className="py-6">
+  <div class="flex justify-center items-center h-screen">
+    <div className="hero-content flex-col lg:flex-row-reverse justify-start lg:justify-start">
+      <div className="text-center lg:text-left">        
+        <Image
+          src="/auth-image.png"
+          alt="Imagen de perritos"
+          width="450"
+          height="500"
+        />
+        {/* <p className="py-6">
           Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
           excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
           id nisi.
-        </p>
+        </p> */}
       </div>
-      <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-300">
+      <div className="card flex-shrink-1 w-full max-w-sm shadow-2xl bg-base-300">
         <div className="card-body">
+        <h1 className="text-3xl font-bold">Registro</h1>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Email</span>
+              <span className="label-text">Nombre y Apellido</span>
             </label>
             <input
               type="text"
-              placeholder="email"
+              placeholder="Ingrese su nombre y apellido"
               className="input input-bordered"
             />
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Password</span>
+              <span className="label-text">E-mail</span>
             </label>
             <input
               type="text"
-              placeholder="password"
+              placeholder="vetcare@gmail"
               className="input input-bordered"
-            />            
+            />
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Repeat Password</span>
+              <span className="label-text">Contraseña</span>
             </label>
             <input
               type="text"
-              placeholder="repeat password"
+              placeholder="Ingrese tu contraseña"
               className="input input-bordered"
-            />            
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Confirmar contraseña</span>
+            </label>
+            <input
+              type="text"
+              placeholder="repita su contraseña"
+              className="input input-bordered"
+            />
           </div>
           <div className="form-control mt-6">
-            <button className="btn btn-neutral">Register</button>
+            <button className="btn btn-neutral">Registrarse</button>
+          </div>
+          <div class="divider">
+            O registrarse con
+          </div>
+          <div className="avatar justify-center gap-8">
+            <div className="rounded">
+              <Image src="/logo.png" width="24" height="24" alt="icono" />
+            </div>
+            <div className="rounded">
+              <Image src="/logo.png" width="24" height="24" alt="icono" />
+            </div>
+            <div className="rounded">
+              <Image src="/logo.png" width="24" height="24" alt="icono" />
+            </div>
           </div>
         </div>
-      </div>
+      </div>      
     </div>
-  )
-  
-  export default Register
+  </div>
+)
+
+export default Register
