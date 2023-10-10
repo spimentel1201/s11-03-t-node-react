fc938f808571d2c04e79f2c2a5741ce33e6efffc
Sonny <spimentelm1201@gmail.com>
2023-10-10 11:04:54 -0500

    feat veterinarians full CRUD added

---
+++ backend/src/routes/veterinarian.routes.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { createVeterinarian, getAllVeterinarians, getVeterinarianById, updateVeterinarian, deleteVeterinarian } from '../controllers/veterinarian.controller';
+import { createVeterinarianValidation, updateVeterinarianValidation } from '../middlewares/validation.middleware';
+import { validMongoId } from '../middlewares/validMongoId.middleware';
+import { authenticate } from '../middlewares/auth.middleware';
+
+const router = express.Router();
+
+// Middleware para todas las rutas de veterinario que requieren un veterinario por ID
+router.param('veterinarianId', validMongoId('veterinarianId'));
+
+// Aplica el middleware de autenticación a las rutas que deseas proteger
+router.use(authenticate);
+
+// Rutas CRUD para veterinarios
+router.post('/', createVeterinarianValidation, createVeterinarian);
+router.get('/', getAllVeterinarians);
+router.get('/:veterinarianId', getVeterinarianById);
+router.put('/:veterinarianId', updateVeterinarianValidation, updateVeterinarian);
+router.delete('/:veterinarianId', deleteVeterinarian);
+
+export default router;
