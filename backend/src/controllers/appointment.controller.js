25b5f63baf73ee3a3bdfc2ca99935cac13563e39
nicoseper89 <nicosepertino@hotmail.com>
2023-10-11 22:42:30 -0300

    refactor: rename and comment code

--- backend/src/controllers/appointment.controller.js
+++ backend/src/controllers/appointment.controller.js
@@ -11,15 +11,18 @@ export const createAppointment = tryCatch(async (req, res) => {
   const { date, reason, cost, notes, petId, clientId } = req.body;
 
   // Verificar si el cliente que se asociara a la cita existe en DB.
-  const existingPet = await Pet.findById(petId);
   const existingClient = await Client.findById(clientId);
 
   if (!existingClient) {
     const error = ErrorApp(`Cliente no encontrado`, 404);
     throw error;
   }
+
+  // Verificar si la mascota que se asociara a la cita existe en DB.
+  const existingPet = await Pet.findById(petId);
+
   if (!existingPet) {
-    const error = ErrorApp(`Mascota no encontrado`, 404);
+    const error = ErrorApp(`Mascota no encontrada`, 404);
     throw error;
   }
 
@@ -35,6 +38,7 @@ export const createAppointment = tryCatch(async (req, res) => {
   // Guarda una nueva cita en la base de datos
   await newAppointment.save();
 
+  // Devuelve una respuesta RESTful desde utils
   sendResponse(res, 201, 'Cita creada con éxito', newAppointment);
 });
 
@@ -45,6 +49,7 @@ export const getAllAppointments = tryCatch(async (req, res) => {
     { path: 'petId' },
   ]);
 
+  // Devuelve una respuesta RESTful desde utils
   sendResponse(res, 200, 'Citas encontradas con éxito', appointments);
 });
 
@@ -58,6 +63,7 @@ export const getAppointmentById = tryCatch(async (req, res) => {
     throw error;
   }
 
+  // Devuelve una respuesta RESTful desde utils
   sendResponse(res, 200, 'Cita encontrada con éxito', appointment);
 });
 
@@ -68,27 +74,24 @@ export const updateAppointment = tryCatch(async (req, res) => {
 
   const appointment = await Appointment.findById(appointmentId);
 
+  // Verificar si la cita existe en DB.
   if (!appointment) {
     const error = ErrorApp(`Cita no encontrada`, 404);
     throw error;
   }
+  
   const updatedAppointment = await Appointment.findByIdAndUpdate(appointmentId, { $set: updateFields }, { new: true });
 
-  sendResponse(res, 200, 'Datos Cita actualizados con éxito', updatedAppointment);
+  // Devuelve una respuesta RESTful desde utils
+  sendResponse(res, 200, 'Cita actualizada con éxito', updatedAppointment);
 });
 
 // Eliminar una cita por ID
 export const deleteAppointment = tryCatch(async (req, res) => {
   const { appointmentId } = req.params;
 
-  /* const deletedAppointment = await Appointment.findByIdAndDelete(appointmentId);
-
-  if (!deletedAppointment) {
-    const error = ErrorApp(`Cita no encontrada`, 404);
-    throw error;
-  } */
   await disableEntity(Appointment, appointmentId, 'Cita');
 
+  // Devuelve una respuesta RESTful desde utils
   sendResponse(res, 200, 'Cita desactivada con éxito');
-  /* res.status(200).json({ message: 'La cita fue eliminada con éxito' }); */
 });
