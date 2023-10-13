f90426613f16d2a08cb83414d9a3c486061a40c1
cereceda1991 <cereceda1991@gmail.com>
2023-10-13 06:39:54 -0500

    feat: add schemas automation

--- backend/src/schemas/appointment.schema.js
+++ backend/src/schemas/appointment.schema.js
@@ -4,39 +4,81 @@ const appointmentSchemaDefinition = new mongoose.Schema({
   date: {
     type: Date,
     required: true,
+    swagger: {
+      type: 'string',
+      description: 'Fecha de la cita',
+      format: 'date',
+    },
   },
   start_time: {
     type: Date,
     required: true,
+    swagger: {
+      type: 'string',
+      description: 'Hora de inicio de la cita',
+      format: 'time',
+    },
   },
   end_time: {
     type: Date,
     required: true,
+    swagger: {
+      type: 'string',
+      description: 'Hora de finalización de la cita',
+      format: 'time',
+    },
   },
   reason: {
     type: String,
     required: true,
+    swagger: {
+      type: 'string',
+      description: 'Motivo de la cita',
+    },
   },
   notes: {
     type: String,
     required: true,
+    swagger: {
+      type: 'string',
+      description: 'Notas adicionales sobre la cita',
+    },
   },
   petId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Pet',
     required: true,
+    swagger: {
+      type: 'string',
+      description: 'ID de la mascota relacionada con la cita',
+    },
   },
   clientId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Client',
     required: true,
+    swagger: {
+      type: 'string',
+      description: 'ID del cliente relacionado con la cita',
+    },
   },
   veterinarianId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Veterinarian',
     required: true,
+    swagger: {
+      type: 'string',
+      description: 'ID del veterinario relacionado con la cita',
+    },
   },
-  isActive: { type: Boolean, default: true },
+  isActive: {
+    type: Boolean,
+    default: true,
+    swagger: {
+      type: 'boolean',
+      description: 'Indica si la cita está activa o inactiva',
+    },
+  },
 });
 
 const Appointment = mongoose.model('Appointment', appointmentSchemaDefinition);
--- backend/src/schemas/pet.schema.js
+++ backend/src/schemas/pet.schema.js
@@ -1,18 +1,58 @@
 import mongoose from 'mongoose';
 
+// Define el esquema para Pet con anotaciones Swagger
 const petSchemaDefinition = new mongoose.Schema({
-  name: { type: String, required: true },
-  age: { type: Number, required: true },
-  size: { type: Number, required: true },
-  weight: { type: Number, required: true },
-  suffering: { type: String },
+  name: {
+    type: String,
+    required: true,
+    swagger: {
+      type: 'string',
+      description: 'Nombre de la mascota',
+    },
+  },
+  age: {
+    type: Number,
+    required: true,
+    swagger: {
+      type: 'number',
+      description: 'Edad de la mascota',
+    },
+  },
+  size: {
+    type: Number,
+    required: true,
+    swagger: {
+      type: 'number',
+      description: 'Tamaño de la mascota',
+    },
+  },
+  weight: {
+    type: Number,
+    required: true,
+    swagger: {
+      type: 'number',
+      description: 'Peso de la mascota',
+    },
+  },
+  suffering: {
+    type: String,
+    swagger: {
+      type: 'string',
+      description: 'Enfermedades o sufrimientos de la mascota',
+    },
+  },
   clientId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Client',
     required: true,
+    swagger: {
+      type: 'string',
+      description: 'ID del cliente dueño de la mascota',
+    },
   },
 });
 
+// Crea el modelo Pet con el esquema definido
 const Pet = mongoose.model('Pet', petSchemaDefinition);
 
 export default Pet;
