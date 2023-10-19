221178566e37e120e8318442423dbf077486d46e
Sonny <spimentelm1201@gmail.com>
2023-10-18 21:40:51 -0500

    feat envio de emails en la actualización y cancelación de citas por parte del cliente.

--- backend/src/services/sendEmail.js
+++ backend/src/services/sendEmail.js
@@ -1,12 +1,12 @@
 import transporter from '../config/node-mailer';
 
-export const sendEmail = async (recipientEmail, subject, htmlContent) => {
-  const mailOptions = {
-    from: process.env.EMAIL_ADDRESS,
-    to: recipientEmail,
-    subject: subject,
-    html: htmlContent,
-  };
+export const sendEmail = async(recipientEmail, subject, htmlContent) => {
+    const mailOptions = {
+        from: process.env.EMAIL_ADDRESS,
+        to: recipientEmail,
+        subject: subject,
+        html: htmlContent,
+    };
 
-  await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions);
 };
