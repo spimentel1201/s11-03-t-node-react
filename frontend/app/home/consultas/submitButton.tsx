0442f1a57e0800fbfc6edaa564e116c17d10a525
Ever Jose Rojas perez <everjosejr@hotmail.com>
2023-10-20 22:04:41 -0500

    add- consultas connection

---
+++ frontend/app/home/consultas/submitButton.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { Toaster } from "react-hot-toast"
+
+
+// import { experimental_useFormStatus as useFormStatus } from 'react-dom'
+export default function SubmitButton() {
+    // const { pending } = useFormStatus()
+  
+   
+    return (
+      <div className="flex flex-col items-center mt-[28px]">
+          <button
+            type="submit"
+            className="w-[182px] h-[62px]  bg-accent 
+        rounded-md border border-accent text-primary text-center"
+        // aria-disabled={pending}
+       
+          >
+            Enviar
+          </button>
+        </div>
+    )
+  }
