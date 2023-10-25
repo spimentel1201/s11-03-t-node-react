165d42d1cbe7c1385deda571078d658c6d5261dc
nicoseper89 <nicosepertino@hotmail.com>
2023-10-24 23:06:44 -0300

    modularize contact section code

--- frontend/app/servicios/page.tsx
+++ frontend/app/servicios/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import { Message, Phone, Location } from "../home/contacto/icons";
 import ServiceCardsContainer from "./components/ServiceCardsContainer";
+import ContactSection from "../components/ContactInfo/ContactSection";
 
 export default function Servicios() {
   return (
@@ -15,26 +15,7 @@ export default function Servicios() {
         </h1>
         <ServiceCardsContainer />
       </div>
-      <section className="bg-background h-[320px] md:h-[186px] bg-cover mb-[76px] flex flex-col md:flex-row justify-center items-center gap-8 md:gap-14 lg:gap-36 font-medium text-[#475467] text-sm lg:text-xl ">
-        <div className="flex gap-6 items-center">
-          <span className="w-10 h-10 bg-[#FF5B2E] rounded-full justify-center items-center inline-flex">
-            <Phone />
-          </span>
-          +1 234 567 890
-        </div>
-        <div className="flex gap-6 items-center">
-          <span className="w-10 h-10 bg-[#FF5B2E] rounded-full justify-center items-center inline-flex">
-            <Message />
-          </span>
-          petcare@gmail.com
-        </div>
-        <div className="flex gap-6 items-center">
-          <span className="w-10 h-10 bg-[#FF5B2E] rounded-full justify-center items-center inline-flex">
-            <Location />
-          </span>
-          Urquiza 1234, CABA
-        </div>
-      </section>
+      <ContactSection />
     </div>
   );
 }
