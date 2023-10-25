043e9679b99d62cbff4bc9c19f65619e4824dfc3
nicoseper89 <nicosepertino@hotmail.com>
2023-10-24 21:22:56 -0300

    add contacts on the page

--- frontend/app/servicios/page.tsx
+++ frontend/app/servicios/page.tsx
@@ -6,6 +6,7 @@ import {
   Vacunacion,
   MedicinaPreventiva,
 } from "../home/icons";
+import { Message, Phone, Location } from "../home/contacto/icons";
 
 interface NuestrosServicios {
   title: string;
@@ -42,29 +43,51 @@ const data: NuestrosServicios[] = [
 
 export default function Servicios() {
   return (
-    <div className="flex flex-col items-center bg-[#f8f0ee] py-5">
-      <div className="flex gap-1 pt-12">
-        <Image src="/foot.svg" width={19} height={16} alt="foot icon" />
-        <p className="font-inter font-bold text-accent">Servicios</p>
-      </div>
-      <h1 className="font-secular text-[20px] md:text-[40px] pb-12">
-        Nuestros servicios
-      </h1>
-      <div className="flex flex-col items-center gap-8">
-        {data.map((item, index) => (
-          <div className="flex bg-[#fffaeb] w-4/5 py-4 rounded-2xl gap-4 px-12">
-            <div>
-              <span>{item.svg && <item.svg />}</span>
+    <>
+      <div className="flex flex-col items-center bg-[#f8f0ee] py-5">
+        <div className="flex gap-1 pt-12">
+          <Image src="/foot.svg" width={19} height={16} alt="foot icon" />
+          <p className="font-inter font-bold text-accent">Servicios</p>
+        </div>
+        <h1 className="font-secular text-[20px] md:text-[40px] pb-12">
+          Nuestros servicios
+        </h1>
+        <div className="flex flex-col items-center gap-8">
+          {data.map((item, index) => (
+            <div className="flex bg-[#fffaeb] w-4/5 py-4 rounded-2xl gap-4 px-12">
+              <div>
+                <span>{item.svg && <item.svg />}</span>
+              </div>
+              <div className="flex flex-col justify-start items-start font-inter">
+                <h2 className="font-inter text-[24px] py-6 font-bold">
+                  {item.title}
+                </h2>
+                <div className="pb-2 text-[16px]">{item.description}</div>
+              </div>
             </div>
-            <div className="flex flex-col justify-start items-start font-inter">
-              <h2 className="font-inter text-[24px] py-6 font-bold">
-                {item.title}
-              </h2>
-              <div className="pb-2 text-[16px]">{item.description}</div>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+      <section className="bg-background h-[149px] md:h-[186px] bg-cover mb-[76px] flex justify-center items-center gap-36 font-medium text-[#475467] text-xl">
+        <div className="flex gap-6 items-center">
+          <span className="w-10 h-10 bg-[#FF5B2E] rounded-full justify-center items-center inline-flex">
+            <Phone />
+          </span>
+          +1 234 567 890
+        </div>
+        <div className="flex gap-6 items-center">
+          <span className="w-10 h-10 bg-[#FF5B2E] rounded-full justify-center items-center inline-flex">
+            <Message />
+          </span>
+          petcare@gmail.com
+        </div>
+        <div className="flex gap-6 items-center">
+          <span className="w-10 h-10 bg-[#FF5B2E] rounded-full justify-center items-center inline-flex">
+            <Location />
+          </span>
+          Urquiza 1234, CABA
+        </div>
+      </section>
+    </>
   );
 }
