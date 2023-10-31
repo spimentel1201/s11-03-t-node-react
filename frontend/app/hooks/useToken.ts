5c17e95c10f284306dec6a8cc70b126ac0ba3413
nicoseper89 <nicosepertino@hotmail.com>
2023-10-31 15:12:50 -0300

    decode token and return its data

--- frontend/app/hooks/useToken.ts
+++ frontend/app/hooks/useToken.ts
@@ -2,18 +2,27 @@
 import { useRouter } from "next/navigation"
 import { useToken } from "../store/token"
 import {useEffect} from "react"
+import { jwtDecode, JwtPayload } from "jwt-decode";
+import { useState } from "react";
 
+interface TokenData extends JwtPayload {
+  clientId: string;
+}
 
 export default function UseToken(){
     
     const token = useToken((state) => state.token)
     const setToken = useToken((state) => state.setToken)
-    const router = useRouter()
-    
+    const router = useRouter();
+    const [data, setData] = useState<TokenData | null>();
+
     useEffect(() => {
         if (typeof window !== 'undefined' && window.localStorage) {
           const Storedtoken = localStorage.getItem('token')
           setToken(Storedtoken)
+          if(Storedtoken){
+            setData(jwtDecode<TokenData>(Storedtoken))
+          }
         }
       }, [setToken]);
      
@@ -21,11 +30,12 @@ export default function UseToken(){
       if (typeof window !== 'undefined' && window.localStorage) {
       localStorage.removeItem('token')
         setToken(null)
+        setData(null);
         router.refresh()
       }    
     }
     
-    return{token, setToken, handleUpdateToken}
+    return{token, data, setToken, handleUpdateToken}
 }
 
 
