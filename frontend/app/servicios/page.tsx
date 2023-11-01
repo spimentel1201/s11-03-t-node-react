7633b699317430027ba31f9769a523acad14ee68
nicoseper89 <nicosepertino@hotmail.com>
2023-11-01 19:58:42 -0300

    fix header styles according to the design

--- frontend/app/servicios/page.tsx
+++ frontend/app/servicios/page.tsx
@@ -1,25 +1,27 @@
-import Image from 'next/image'
-import ServiceCardsContainer from './components/ServiceCardsContainer'
-import ContactSection from '../components/ContactInfo/ContactSection'
-import Footer from '../components/footer/footer'
-import NuestroTeam from '../home/nuestroTeam'
+import Image from "next/image";
+import ServiceCardsContainer from "./components/ServiceCardsContainer";
+import ContactSection from "../components/ContactInfo/ContactSection";
+import Footer from "../components/footer/footer";
+import NuestroTeam from "../home/nuestroTeam";
 
 export default function Servicios() {
   return (
     <>
-      <div className="flex flex-col items-center bg-[#f8f0ee] py-5">
-        <div className="flex gap-1 pt-12">
-          <Image src="/foot.svg" width={19} height={16} alt="foot icon" />
-          <p className="font-inter font-bold text-accent">Servicios</p>
+      <div className="flex flex-col items-center bg-[#f8f0ee] pt-0 md:pt-8">
+        <div className="flex flex-col items-center py-16 gap-8 md:gap-1">
+          <div className="flex gap-1">
+            <Image src="/foot.svg" width={19} height={16} alt="foot icon" />
+            <p className="font-inter font-bold text-accent">Servicios</p>
+          </div>
+          <h1 className="font-secular text-[20px] md:text-[40px]">
+            Nuestros servicios
+          </h1>
         </div>
-        <h1 className="font-secular text-[20px] md:text-[40px] pb-12">
-          Nuestros servicios
-        </h1>
         <ServiceCardsContainer />
       </div>
       <NuestroTeam rows={2} />
       <ContactSection />
       <Footer />
     </>
-  )
+  );
 }
