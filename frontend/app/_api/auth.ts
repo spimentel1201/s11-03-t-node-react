085f56e75024ad47a64346a7e489d2843146421a
Sonny Pimentel <spimentelm1201@gmail.com>
2024-01-05 20:07:26 -0600

    Update auth.ts
    Signed-off-by: Sonny Pimentel <spimentelm1201@gmail.com>

--- frontend/app/_api/auth.ts
+++ frontend/app/_api/auth.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const BASE_URL = 'https://s11-03-react-node-production.up.railway.app/api/v1'
+const BASE_URL = 'https://vetcare-qwzz.onrender.com/api/v1'
 // const BASE_URL = 'localhost:3001/api/v1'
 
 const api = axios.create({
